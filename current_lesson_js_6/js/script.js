10019dd97d4f7a5ddd5b658a882628f0b4fb5cd2
klejnov <petro.vich@list.ru>
2018-02-04 00:41:34 +0300

    Домашнее задание. JavaScript

--- current_lesson_js_6/js/script.js
+++ current_lesson_js_6/js/script.js
@@ -18,7 +18,55 @@ function randomButton() {
     alert('Вспомните пять чисел показанных ранее и введите каждое из них последовательно в правильном порядке');
 
     for (var i = 1, userNumber = ''; i < 6; i++) {
-        userNumber += ' \n Число ' + i + ': ' + prompt('Введите число ' + i, '');
+
+        var userNumberPrompt = prompt('Введите число ' + i, '');
+
+// Удалим пробелы с начала и конца строки, либо удалим все пробелы и сделаем пустую строку ''.
+// Но сделаем условие, чтобы первый раз отработала кнопка Отмена, т.к. trim "мешает" работе.
+
+        if (userNumberPrompt != null) {
+            userNumberPrompt = userNumberPrompt.trim();
+        }
+
+// Цикл на проверку:
+// нажатие на кнопку Отмена
+// введено ли значение
+// введен текст или число
+// не выходит ли число из диапозона
+
+        while (userNumberPrompt == null || userNumberPrompt == '' || isNaN(userNumberPrompt) || userNumberPrompt > 9 || userNumberPrompt < 0 || Number.isInteger(+userNumberPrompt) == false)  {
+            switch (true) {
+                case userNumberPrompt == null:
+                    alert('Вы нажали Отмена');
+                    userNumberPrompt = prompt('Попробуйте ввести число заново', '').trim();
+                    break;
+                case userNumberPrompt == '':
+                    alert('Вы ничего не ввели');
+                    userNumberPrompt = prompt('Попробуйте ввести число заново', '').trim();
+                    break;
+                case isNaN(userNumberPrompt):
+                    alert('Вы ввели не число');
+                    userNumberPrompt = prompt('Попробуйте ввести число, а не текст', '').trim();
+                    break;
+                case userNumberPrompt > 9:
+                    alert('Вы ввели число больше 10');
+                    userNumberPrompt = prompt('Попробуйте ввести число в верном диапазоне', '').trim();
+                    break;
+                case userNumberPrompt < 0:
+                    alert('Вы ввели число меньше 0');
+                    userNumberPrompt = prompt('Попробуйте ввести число в верном диапазоне', '').trim();
+                    break;
+                case Number.isInteger(+userNumberPrompt) == false:
+                    alert('Введите целое число');
+                    userNumberPrompt = prompt('Необходимо ввести целое число!', '').trim();
+                    break;
+                default:
+                    alert('Что-то пошло не так :)');
+                    userNumberPrompt = prompt('Попробуйте ввести число заново', '').trim();
+            }
+
+        }
+        userNumber += ' \n Число ' + i + ': ' + userNumberPrompt;
         alert(userNumber);
         console.log('число ' + i + ' = ' + userNumber);
     }
