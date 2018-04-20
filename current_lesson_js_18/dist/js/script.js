256fbbf0df6578d0ba3331736da4f0f61a8285a8
klejnov <petro.vich@list.ru>
2018-04-20 23:17:58 +0300

    Домашнее задание. JavaScript

---
+++ current_lesson_js_18/dist/js/script.js
@@ -0,0 +1,102 @@
+;
+
+
+$(function () {
+
+    $('.tbl-header th').on('click', '.fa-filter', function () {
+
+        $(event.target).parent().find('input').slideToggle(500);
+        console.log($(event.target).parent());
+    });
+
+    $('.tbl-header th').on('click', 'span, [class^="fa fa-sort"]', function () {
+
+        // определим позицию input
+        var position = $(event.target).parent().index() + 1;
+        console.log(position);
+
+        var flag = $(event.target).parent().find('.fa').first().is(':visible');
+
+        console.log(flag);
+        if (flag) {
+            $(event.target).parent().find('.fa-sort-alpha-desc').show();
+            $(event.target).parent().find('.fa-sort-alpha-asc').hide();
+
+            $('.tbl-content tr').sort(function (a, b) { // сортируем
+                if (position == 1) {
+                    return $(b).find("td:nth-child(" + position + ")").text().localeCompare($(a).find("td:nth-child(" + position + ")").text());
+                } else {
+                    return $(b).find("td:nth-child(" + position + ")").text() - $(a).find("td:nth-child(" + position + ")").text();
+                }
+            }).appendTo('.tbl-content table tbody'); // возвращаем в контейнер
+
+
+            $(event.target).parent().find('.fa-sort-numeric-desc').show();
+            $(event.target).parent().find('.fa-sort-numeric-asc').hide();
+        } else {
+            $(event.target).parent().find('.fa-sort-alpha-asc').show();
+            $(event.target).parent().find('.fa-sort-alpha-desc').hide();
+
+            $('.tbl-content tr').sort(function (a, b) { // сортируем
+                if (position == 1) {
+                    return $(a).find("td:nth-child(" + position + ")").text().localeCompare($(b).find("td:nth-child(" + position + ")").text());
+                } else {
+                    return $(a).find("td:nth-child(" + position + ")").text() - $(b).find("td:nth-child(" + position + ")").text();
+                }
+            }).appendTo('.tbl-content table tbody'); // возвращаем в контейнер
+
+            $(event.target).parent().find('.fa-sort-numeric-asc').show();
+            $(event.target).parent().find('.fa-sort-numeric-desc').hide();
+        }
+
+    });
+
+
+    $('input').keyup(function () {
+
+        var country = $('input[name="country"]').val().toUpperCase();
+        var consumption = $('input[name="consumption"]').val().toUpperCase();
+        var beer = $('input[name="beer"]').val().toUpperCase();
+        var wine = $('input[name="wine"]').val().toUpperCase();
+        var forecast = $('input[name="forecast"]').val().toUpperCase();
+
+        $(".tbl-content tr").show(1000);
+
+        $(".tbl-content tr").each(function (indx, element) {
+
+            var countryTd = $(element).find('td:nth-child(1)').text().toUpperCase();
+            var consumptionTd = $(element).find('td:nth-child(2)').text().toUpperCase();
+            var beerTd = $(element).find('td:nth-child(3)').text().toUpperCase();
+            var wineTd = $(element).find('td:nth-child(4)').text().toUpperCase();
+            var forecastTd = $(element).find('td:nth-child(5)').text().toUpperCase();
+
+            console.log(countryTd);
+            console.log(!(countryTd.indexOf(country) + 1));
+
+            if (!(countryTd.indexOf(country) + 1) ||
+                !(consumptionTd.indexOf(consumption) + 1) ||
+                !(beerTd.indexOf(beer) + 1) ||
+                !(wineTd.indexOf(wine) + 1) ||
+                !(forecastTd.indexOf(forecast) + 1)) {
+                $(".tbl-content tr").eq(indx).hide();
+                //console.log(indx);
+            }
+        });
+
+    });
+
+
+});
+
+
+//вывод кода в textarea (подключен jQuery v3.3.1 для работы с ajax)
+var code_js;
+$.ajax({
+    url: "js/script.js",
+    dataType: "text",
+    async: true,
+    success: function (msg) {
+        code_js = msg;
+        document.getElementById("code").innerHTML = code_js;
+    }
+});
---
+++ current_lesson_js_18/gulpfile.js
@@ -0,0 +1,124 @@
+// Команды Gulp: https://simplamarket.com/blog/ispolzovanie-gulp-chast-1---ustanovka
+
+var	gulp					= require('gulp'), // Подключаем Gulp
+    sass					= require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync  = require('browser-sync'), // Подключаем Browser Sync
+    concat				= require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify					= require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    cssnano				= require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename				= require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del						= require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin			= require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant			= require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache					= require('gulp-cache'), // Подключаем библиотеку кеширования
+    autoprefixer		= require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
+    notify					= require( 'gulp-notify' ), // Вывод ошибок и чтобы не завершался gulp в консоли
+    sftp						= require('gulp-sftp'); // загрузка на SFTP
+
+gulp.task('sass', function(){ // Создаем таск Sass
+    return gulp.src('assets/sass/**/*.scss') // Берем источник
+        .pipe(sass() // Преобразуем Sass в CSS посредством gulp-sass
+            .on( 'error', notify.onError(
+                {
+                    message: "<%= error.message %>",
+                    title  : "Sass Error!"
+                } ) )
+        )
+        .pipe(cssnano({autoprefixer: {
+            browsers:['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+            add: true
+        }})) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+        .pipe(gulp.dest('assets/css')) // Выгружаем результата в папку assets/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'assets' // Директория для сервера - assets
+        },
+        notify: false, // Отключаем уведомления
+        // tunnel: true,
+        // tunnel: "klejnovd" //Demonstration page: http://klejnov.localtunnel.me
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+        'assets/libs/jquery/jquery-3.3.1.js', // Берем jQuery
+        //'assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+    ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('assets/js')); // Выгружаем в папку assets/js
+});
+
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src([
+        'assets/css/table.css'
+    ]) // Выбираем файл для минификации
+        .pipe(cssnano({autoprefixer: {
+            browsers:['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+            add: true
+        }})) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('assets/css')); // Выгружаем в папку assets/css
+
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+    gulp.watch('assets/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('assets/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('assets/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('assets/img/**/*') // Берем все изображения из assets
+        .pipe(cache(imagemin({ // С кешированием
+            // .pipe(imagemin({ // Сжимаем изображения без кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))/**/)
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+
+    var buildCss = gulp.src(['assets/css/**/*.css', '!assets/css/table.css']) // Переносим библиотеки в продакшен
+        .pipe(gulp.dest('dist/css'));
+
+    var buildFonts = gulp.src('assets/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('dist/fonts'));
+
+    var buildJs = gulp.src('assets/js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/js'));
+
+    var buildHtml = gulp.src('assets/*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function (callback) {
+    return cache.clearAll();
+});
+
+gulp.task('deploy', ['build'], function () {
+    return gulp.src('dist/**')
+        .pipe(sftp({
+            host: 'ipdes.by',
+            user: 'ipdesby',
+            pass: '***',
+            port: 52222,
+            remotePath: '/home/ipdesby/public_html/test'
+        }));
+});
+
+gulp.task('default', ['watch']);
